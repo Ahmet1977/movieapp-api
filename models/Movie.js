13125f073a525381ddf58b186cd2f1082072c635
Ahmet1977 <79558897+Ahmet1977@users.noreply.github.com>
2021-07-27 23:48:05 -0400

    revised

--- models/Movie.js
+++ models/Movie.js
@@ -9,6 +9,7 @@ const MovieSchema = new Schema({
     maxlength:[50,'The field `{PATH}` must be less than {MAXLENGTH} characters']
   },
   category:{type:String, maxlength:30, minlength:1},
+  
   year:{type:Number, min:1850, max:2100},
   imdb_score:{type:Number, max:10, min:0},
   country:{type:String, maxlength:60, minlength:2},
